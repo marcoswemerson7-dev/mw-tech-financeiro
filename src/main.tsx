4ce9950a4050d8b4a87c1d26b85ab19faacd7ff1
marcoswemerson7-dev <marcoswemerson7@gmail.com>
2026-08-28 17:04:18 -0300

    feat: substituir alerts por toasts no sistema

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,15 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import ToastHost, { installToastAlerts } from './components/ToastHost.tsx'
+
+installToastAlerts()
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter><App /></BrowserRouter>
+    <BrowserRouter>
+      <App />
+      <ToastHost />
+    </BrowserRouter>
   </StrictMode>,
 )
